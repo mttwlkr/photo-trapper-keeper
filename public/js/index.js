11decb24ab3017eacf48de6e227a7c23138d561a
Matt Walker <mttwlkr@gmail.com>
2018-05-21 10:50:49 -0600

    Fetching existing images and appending them

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
+// $('.add-photo-button').on('click',)
+
+// function addNewPhoto(event) {
+//   event.preventDefault()
+//   const userTitle = $('#add-photo-title').val()
+//   const userURL = $('#add-photo-url').val()
+//   console.log(userURL)
+// }
+
+async function fetchPhotos() {
+  const response = await fetch('/api/v1/photos')
+  const photos = await response.json()
+  return photos
+}
+
+async function appendAllPhotos() {
+  const photos = await fetchPhotos();
+  const displayPhotos = photos.map( photo => {
+    return (`<div id=${photo.id}><h2 class='display-photo-title'>${photo.title}</h2><img class='display-photo-image' src='${photo.url}' /></div>`)
+  })
+  // displayPhotos.forEach( div => {
+  //   console.log(div)
+  //   $('.display-albums').append(div)
+  // })
+  $('.display-albums').append(displayPhotos)
+}
+
+
 $(document).ready( () => {
-  console.log('hi')
+  appendAllPhotos()
 });
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];      
+const database = require('knex')(configuration);  
+
 app.use(express.static('public'))
 app.use(bodyParser.json()) 
 
@@ -10,7 +14,17 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
   app.use(express.static(path.join(__dirname, 'public')));
-})            
+})
+
+app.get('/api/v1/photos', (request, response) => {
+  database('photos').select()                                  
+    .then((photos) => {                                   
+      response.status(200).json(photos);              
+    })                
+    .catch((error) => {                                             
+      response.status(500).json(error);                         
+    });
+})          
 
 app.listen(app.get('port'), () => {
   console.log(`Photo Trapper Keeper is running on ${app.get('port')}`)
