ae7b8aa2279ba88f682f03816575b0c1421a0258
Matt Walker <mttwlkr@gmail.com>
2018-05-21 11:35:30 -0600

    Add post request, appendSinglePhoto to page and clear input fields

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,31 @@
-// $('.add-photo-button').on('click',)
+$('#add-photo-button').click(addNewPhoto)
 
-// function addNewPhoto(event) {
-//   event.preventDefault()
-//   const userTitle = $('#add-photo-title').val()
-//   const userURL = $('#add-photo-url').val()
-//   console.log(userURL)
-// }
+async function addNewPhoto() {
+  event.preventDefault();
+  const userTitle = $('#add-photo-title').val()
+  const userURL = $('#add-photo-url').val()
+
+  const response = await fetch('/api/v1/photos', {
+    method: "POST",
+    body: JSON.stringify({title: userTitle, url: userURL}),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  const responsePhoto = await response.json();
+  if (responsePhoto.newPhoto.id) { resetFields() }
+  appendSinglePhoto(responsePhoto)
+}
+
+function resetFields() {
+  $('#add-photo-title').val('')
+  $('#add-photo-url').val('')
+}
+
+async function appendSinglePhoto(photo) {
+  const displayPhoto = `<div id=${photo.newPhoto.id}><h2 class='display-photo-title'>${photo.newPhoto.title}</h2><img class='display-photo-image' src='${photo.newPhoto.url}' /><button class='display-photo-delete-button'>X</button></div>`
+  $('.display-albums').append(displayPhoto)
+}
 
 async function fetchPhotos() {
   const response = await fetch('/api/v1/photos')
@@ -18,13 +38,8 @@ async function appendAllPhotos() {
   const displayPhotos = photos.map( photo => {
     return (`<div id=${photo.id}><h2 class='display-photo-title'>${photo.title}</h2><img class='display-photo-image' src='${photo.url}' /><button class='display-photo-delete-button'>X</button></div>`)
   })
-  // displayPhotos.forEach( div => {
-  //   console.log(div)
-  //   $('.display-albums').append(div)
-  // })
   $('.display-albums').append(displayPhotos)
 }
-
 
 $(document).ready( () => {
   appendAllPhotos()
--- server.js
+++ server.js
@@ -24,7 +24,23 @@ app.get('/api/v1/photos', (request, response) => {
     .catch((error) => {                                             
       response.status(500).json(error);                         
     });
-})          
+})
+
+app.post('/api/v1/photos', (request, response) => {
+  if (!request.body) {
+    return response.status(422).send({Error: "Missing Information"})
+  }
+  database('photos').insert(request.body, ['title', 'url', 'id'])
+    .then( photo => {
+      console.log(photo)
+      response.status(201).json({
+        newPhoto: photo[0]
+      })
+    })
+    .catch( error => {
+      response.status(500).json({ error })
+    })
+})       
 
 app.listen(app.get('port'), () => {
   console.log(`Photo Trapper Keeper is running on ${app.get('port')}`)
